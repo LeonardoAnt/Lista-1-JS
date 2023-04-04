15426e43761c8b400c3720d245f7b2e7dc629534
LeonardoAnt <128732497+LeonardoAnt@users.noreply.github.com>
2023-04-04 15:28:55 -0300

    exercícios

---
+++ exercicios lista 1/exercicio1.js
@@ -0,0 +1,19 @@
+console.log("Sequencia em For")
+
+
+let la = document.getElementById("la")
+let resposta = document.getElementById('resultadoFinal')
+for (let i = 1; i <= 10; i++) {
+
+
+    console.log(i);
+  }
+  console.log("Sequencia em While")
+
+
+  let i = 1;
+ 
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
